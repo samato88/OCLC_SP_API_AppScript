f7ffae4ad0972ccb953fc1d6ee8b9cb02a3a6709
sara amato <samato@willamette.edu>
2022-11-11 11:15:25 -0800

    Moved sheet update code to separate file, just for ease of reading

--- Code.js
+++ Code.js
@@ -133,7 +133,7 @@ if (startingRow < 2) {
    if (form.WCHoldings == true) {edition = "Any"} else { edition = "Same"} ;
    var columnHeaders = [["WorldCat OCLC", SPP + " Retentions", "Retained By Symbol", "Hathi", "IA", "US Holdings (" + edition + " edition) in WorldCat", "Merged OCLC numbers", 	"Hathi ID", "Hathi Title", "OCLC Title"]] ;
 
-  try {// wondering if this will catch time outs - try/catch around all lookups
+  try {// wondering if this will catch time outs - try/catch around all lookups -Nope.
    var x = 1;
    if (startingRow != null && startingRow != "" && startingRow !=1) 
     { 
@@ -223,115 +223,20 @@ if (startingRow < 2) {
         } // end form.ia  
        
       } // end if oclc cell not blank
-    } // end for each OCLC
-  
+    } // end for each OCLC 
+
   } catch(err) {
      Logger.log(err);
      ui.alert(err); // will this catch Exceeded maximum execution time
      // need to return here?  write out what it got?... but doesn't continue on
    } // end catch
 
-  // update sheet, would be faster to do as a multidimensional array
-  try {
-    var lock = LockService.getScriptLock();
-    try { 
-    lock.tryLock(30000)  //This method has no effect if the lock has already been acquired.https://developers.google.com/apps-script/reference/lock/lock
-    
-    if (form.hathi) {
-      updateSheetColumn(startingRow, numRows, hathiColumn, "E", dataSheet) ; 
-      updateSheetColumn(startingRow, numRows, hathiIdColumn, "I", dataSheet) ; 
-      updateSheetColumn(startingRow, numRows, hathiTitleColumn, "J", dataSheet) ; 
-    }
-    if (form.ia)   {updateSheetColumn(startingRow, numRows, iaColumn, "F", dataSheet) ; }
-
-  if (form.worldcatretentions) { // this is the checkbox for SPP
-      updateSheetColumn(startingRow, numRows, sppColumn,        "C", dataSheet) ; 
-      updateSheetColumn(startingRow, numRows, retainersColumn,   "D", dataSheet) ; 
-      updateSheetColumn(startingRow, numRows, currentOCLCColumn, "B", dataSheet) ; 
-      if (form.WCHoldings) {
-        updateSheetColumn(startingRow, numRows, usHoldingsColumn, "G", dataSheet) ;
-      }
-      if (form.WCData) { 
-        updateSheetColumn(startingRow, numRows, mergedOCLCColumn, "H", dataSheet) ;
-        updateSheetColumn(startingRow, numRows, worldcatTitleColumn, "K", dataSheet) ; }
-    } // end if form.worldcatretentions
-  
-    dataSheet.getRange("B1:K1").setValues(columnHeaders); // set column headers
-    dataSheet.getRange("B1:K1").setFontWeight("bold");
-    dataSheet.getRange("B1:K1").setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
-
-
-    lock.releaseLock();
-    } catch {
-          ui.alert("Failed to sheet lock - reload sheet and try again.") // not sure this is a good idea
-          return ;
-    }
-  } // end try getting a lock and updating sheet
-  catch { // catch failed lock
-    ui.alert("Failed to lock sheet - reload sheet and try again.") // slightly different error message for debugging where failed
-    return ;
-  }
-
- 
-  if (form.sendEmail) {// send plain generic email if requested ( html alternative https://blog.gsmart.in/google-apps-script-send-html-email/)
-    var donetime = new Date();
-    var emailAddress = form.emailAddress ;
-    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
-    if(emailPattern.test(emailAddress) == false) {
-      //Logger.log("Invalid Email Address Entered. " + emailAddress);
-      ui.alert("Invalid email address entered. No mail will be sent")
-    } else {
-      var subject = "Shared Print Retentions Spreadsheet Complete";
-      var message = "Shared Print Retentions Spreadsheet lookup completed at " + donetime.toUTCString(); 
-      message += "\nThis message sent from the sheet: " + SpreadsheetApp.getActive().getUrl(); // not sure if this is right sheet
-
-      try {
-        MailApp.sendEmail(emailAddress, subject, message); 
-      } catch(e) {
-        ui.alert("Error sending email. No mail will be sent")
-        Logger.log("Error with email (" + emailAddress + "). " + e);
-      } // end catch email
-    } // end else is valid email pattern
-  } // end if form sendEmail 
-
+  // update sheet was inline here
+  updateSheet(form, startingRow, numRows, dataSheet, hathiColumn,hathiIdColumn, hathiTitleColumn, iaColumn, sppColumn, retainersColumn, currentOCLCColumn, usHoldingsColumn,mergedOCLCColumn, worldcatTitleColumn, columnHeaders)
    //Logger.log('Script done');
-   //PropertiesService.getScriptProperties().setProperty('run', 'done'); //leap of faith that above is synchronous
- 
+   //PropertiesService.getScriptProperties().setProperty('run', 'done'); //leap of faith that above is synchronous 
 
 } // end start lookup
- 
-//===================================================================================================
-function updateSheetColumn(startingRow, rows, newValues, column, sheet) {  //https://developers.google.com/apps-script/guides/support/best-practices
-
-numrowsremove = startingRow-2
-newValues.splice(0, numrowsremove);
-
-var formatColumn = sheet.getRange(column + ":" + column);
-formatColumn.setNumberFormat("@"); // set column to be a text, not number, column - for merged OCLC numbers especially!
-   
-var rangeRows = (rows-(startingRow-1)+1)  // number of rows to fetch 
-var sheetRange = column + startingRow + ":" + column + (rows + 1) ;
-var allRange = sheet.getRange(sheetRange);// e.g. sheet.getRange("C2:C600") 
-    
-var updateValues = [] ; // create new array for update [column][index]
-for (counter = 0; counter < newValues.length; ++counter) {   updateValues[counter] = new Array(1); } ;
-
-for (counter = 0; counter < newValues.length; ++counter) {    
-  if (typeof newValues[counter] == "undefined") { newValues[counter]= "";}
-  updateValues[counter][0] = newValues[counter];  
-}  // end for counter < new values 
-
-allRange.setValues(updateValues) ; // actually update the sheet
-   
-} // end updateSheetColumn
-
-//===================================================================================================  
-function getPercentDone() { // never got this working - if any has ideas would love to hear them
-  //ui.alert("PD Property: " + PropertiesService.getUserProperties().getProperty('percentDone')); // this seems to always be 100 when we get here
-  return PropertiesService.getUserProperties().getProperty('percentDone');
-
-  //return percentDone ;
-}
 //===================================================================================================  
 
 function getApiService() {  //https://github.com/gsuitedevs/apps-script-oauth2/blob/master/samples/TwitterAppOnly.gs
@@ -363,4 +268,8 @@ function reset() { // Reset the authorization state, so that it can be re-tested
   getApiService().reset();
 }
 //===================================================================================================    
-
+function getPercentDone() { // never got this working - if any has ideas would love to hear them
+  //ui.alert("PD Property: " + PropertiesService.getUserProperties().getProperty('percentDone')); // this seems to always be 100 when we get here
+  return PropertiesService.getUserProperties().getProperty('percentDone');
+  //return percentDone ;
+}
---
+++ UpdateSheet.js
@@ -0,0 +1,90 @@
+function updateSheet(form, startingRow, numRows, dataSheet, hathiColumn,hathiIdColumn, hathiTitleColumn, iaColumn, sppColumn, retainersColumn, currentOCLCColumn, usHoldingsColumn,mergedOCLCColumn, worldcatTitleColumn, columnHeaders) {
+  // update sheet, would be faster to do as a multidimensional array
+  try {
+    var lock = LockService.getScriptLock();
+    try { 
+    lock.tryLock(30000)  //This method has no effect if the lock has already been acquired.https://developers.google.com/apps-script/reference/lock/lock
+
+    if (form.hathi) {
+      updateSheetColumn(startingRow, numRows, hathiColumn, "E", dataSheet) ; 
+      updateSheetColumn(startingRow, numRows, hathiIdColumn, "I", dataSheet) ; 
+      updateSheetColumn(startingRow, numRows, hathiTitleColumn, "J", dataSheet) ; 
+    }
+    if (form.ia)   {updateSheetColumn(startingRow, numRows, iaColumn, "F", dataSheet) ; }
+
+  if (form.worldcatretentions) { // this is the checkbox for SPP
+      updateSheetColumn(startingRow, numRows, sppColumn,        "C", dataSheet) ; 
+      updateSheetColumn(startingRow, numRows, retainersColumn,   "D", dataSheet) ; 
+      updateSheetColumn(startingRow, numRows, currentOCLCColumn, "B", dataSheet) ; 
+      if (form.WCHoldings) {
+        updateSheetColumn(startingRow, numRows, usHoldingsColumn, "G", dataSheet) ;
+      }
+      if (form.WCData) { 
+        updateSheetColumn(startingRow, numRows, mergedOCLCColumn, "H", dataSheet) ;
+        updateSheetColumn(startingRow, numRows, worldcatTitleColumn, "K", dataSheet) ; }
+    } // end if form.worldcatretentions
+  
+    dataSheet.getRange("B1:K1").setValues(columnHeaders); // set column headers
+    dataSheet.getRange("B1:K1").setFontWeight("bold");
+    dataSheet.getRange("B1:K1").setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
+
+
+    lock.releaseLock();
+    } catch {
+          ui.alert("Failed to sheet lock - reload sheet and try again.") // not sure this is a good idea
+          return ;
+    }
+  } // end try getting a lock and updating sheet
+  catch { // catch failed lock
+    ui.alert("Failed to lock sheet - reload sheet and try again.") // slightly different error message for debugging where failed
+    return ;
+  }
+
+ 
+  if (form.sendEmail) {// send plain generic email if requested ( html alternative https://blog.gsmart.in/google-apps-script-send-html-email/)
+    var donetime = new Date();
+    var emailAddress = form.emailAddress ;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+    if(emailPattern.test(emailAddress) == false) {
+      //Logger.log("Invalid Email Address Entered. " + emailAddress);
+      ui.alert("Invalid email address entered. No mail will be sent")
+    } else {
+      var subject = "Shared Print Retentions Spreadsheet Complete";
+      var message = "Shared Print Retentions Spreadsheet lookup completed at " + donetime.toUTCString(); 
+      message += "\nThis message sent from the sheet: " + SpreadsheetApp.getActive().getUrl(); // not sure if this is right sheet
+
+      try {
+        MailApp.sendEmail(emailAddress, subject, message); 
+      } catch(e) {
+        ui.alert("Error sending email. No mail will be sent")
+        Logger.log("Error with email (" + emailAddress + "). " + e);
+      } // end catch email
+    } // end else is valid email pattern
+  } // end if form sendEmail 
+
+}
+//===================================================================================================
+function updateSheetColumn(startingRow, rows, newValues, column, sheet) {  //https://developers.google.com/apps-script/guides/support/best-practices
+
+numrowsremove = startingRow-2
+newValues.splice(0, numrowsremove);
+
+var formatColumn = sheet.getRange(column + ":" + column);
+formatColumn.setNumberFormat("@"); // set column to be a text, not number, column - for merged OCLC numbers especially!
+   
+var rangeRows = (rows-(startingRow-1)+1)  // number of rows to fetch 
+var sheetRange = column + startingRow + ":" + column + (rows + 1) ;
+var allRange = sheet.getRange(sheetRange);// e.g. sheet.getRange("C2:C600") 
+    
+var updateValues = [] ; // create new array for update [column][index]
+for (counter = 0; counter < newValues.length; ++counter) {   updateValues[counter] = new Array(1); } ;
+
+for (counter = 0; counter < newValues.length; ++counter) {    
+  if (typeof newValues[counter] == "undefined") { newValues[counter]= "";}
+  updateValues[counter][0] = newValues[counter];  
+}  // end for counter < new values 
+
+allRange.setValues(updateValues) ; // actually update the sheet
+   
+} // end updateSheetColumn
+//===================================================================================================
